12e9ed1f26da39a82fc51880a00031a95ba227d9
Sean Beach <sean.beach1@yahoo.com>
2019-11-01 01:25:01 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,20 +1,25 @@
 def addition(num1, num2)
-
+  sum = {num1 + num2}
+  puts sum
 end
 
 def subtraction(num1, num2)
-
+  difference = {num1 - num2}
+  puts difference
 end
 
 def division(num1, num2)
-
+  quotient = {num1 / num2}
+  puts quotient
 end
 
 def multiplication(num1, num2)
-
+  product = (num1 * num2)
+  puts product
 end
 
 def modulo(num1, num2)
+  
 
 end
 
